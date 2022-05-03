b23e7c53da38908f43af2554302add4ba2a363f8
Alexander Rovang <DXRovang@gmail.com>
2022-05-03 15:51:09 -0400

    forEach

--- src/App.js
+++ src/App.js
@@ -3,27 +3,58 @@ import Input from './Input'
 import {useState} from 'react'
 function App() {
 
-  const [numbers, setNumbers] = useState([5,26,7,300,71,8])
-  // const sorted = numbers.sort((a,b)=>a-b).map(n=>n + "  ")
+  const nn = [5,26,7,300,71,8]
+  const [numbers, setNumbers] = useState(nn)
+  const [words, setWords] = useState(["blue", "red", "green"])
 
   const spread = () =>{
-    setNumbers(numbers=>[...numbers, 909])
+    setNumbers(numbers=>[909,...numbers])
   }
+
+  const changeWord = ()=>{
+    let wc = []
+    words.forEach(w => { 
+      if (w === "red"){
+        w = "pink"
+      }
+      wc.push(w)
+    })
+    console.log(wc)
+    setWords(wc)
+
+    // let r = words.map(w => { 
+    //   if (w === "red"){
+    //     w = "pink"
+    //   }
+    // })
+    // setWords(r)
+  }
+
+  const spl = ()=>{
+    let n = numbers
+    console.log(n.slice(2,5))
+  }
+
   return (
     <>
       <Input/>
       <div>
-        {numbers.map(n=>n + "  ")}
+        Unsorted: {numbers.map(n=>n + "  ")}
       </div>
       <div>
         {numbers.filter(n=>(n>10)).map(n=>n + "  ")}
       </div>
       <div>
-        {/* {sorted} */}
-      </div>
-      <div>
         <button onClick={spread}>spread</button>
       </div>
+      <div>
+      <button onClick={spl}>spl</button>
+      </div>
+      <div>
+        {words.map(w=><div>{w}</div>)}
+        <button onClick={changeWord}>Change</button>
+      </div>
+
     </>
   );
 }
